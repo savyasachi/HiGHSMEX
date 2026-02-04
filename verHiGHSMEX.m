a87ccbbf962914afe8cf77f0a50dfba2da751a67
Ray Zimmerman <ray.zimmerman@poweranalyticssoftware.com>
2026-02-03 17:10:45 -0800

    Update MEX version number to reflect new functionality.

--- verHiGHSMEX.m
+++ verHiGHSMEX.m
@@ -6,6 +6,6 @@ function ver = verHiGHSMEX()
 % Covered by the MIT License (see LICENSE file for details).
 % See https://github.com/savyasachi/HiGHSMEX for more information.
 
-ver = "1.2.0";
+ver = "1.3.0";
 
 % EOF
